cf7de7e1f0f4ebb0e26f42c147880a09afb05344
San'Quan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-05-14 12:16:49 -0400

    Enable card flipping, add 3D effect to cards at "/cards" route

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -9,6 +10,7 @@ import CardToolbar from "./CardToolbar";
 
 const Card = () => {
   const [card, setCard] = useState();
+  const [flipped, setFlipped] = useState(false);
   const dispatch = useDispatch();
   const { cardId } = useParams();
 
@@ -21,66 +23,91 @@ const Card = () => {
   const getImageSourceFromType = (type) => `../images/pokeme-types/${card.type ? `${type.toLowerCase()}.svg` : "bug.svg"}`;
   const getBgImageSourceFromType = (type) => `url(../images/pokeme-types/${card.type ? `${type.toLowerCase()}-bg.png)` : "url(../images/pokeme-types/bug-bg.png"}`;
   const getPokemeImage = () => `${card.photoUrl}`;
+  const toggleFlipHorizontally = (e) => setFlipped(!flipped);
+  const getCardBackgroundImage = () => {
+    if (!card.isSpecial) return getBgImageSourceFromType(card.type);
+    return "none";
+  };
 
   return (
     <div className="card-positioner">
       <CardToolbar cardId={cardId} />
-      <Tilt
-        className="parallax"
-        perspective={1000}
-        glareEnable
-        glareMaxOpacity={0.45}
-        scale={1.02}
-      >
-        <div
-          className={`card${card.isSpecial ? " special" : ""}`}
-          style={{ backgroundImage: !card.isSpecial ? getBgImageSourceFromType(card.type) : "none", }}
+        <Tilt
+          className="parallax"
+          perspective={1000}
+          glareEnable
+          glareMaxOpacity={0.45}
+          scale={1.02}
+          flipHorizontally={flipped}
+          transitionSpeed={1500}
         >
-          <header className="card__header">
-            <h3 className="card__name">{card.name}</h3>
-            <div className="card__header-right">
-              <span className="card__hit-points">{card.hitPoints}</span>
-                <img className="card__type-badge icon" src={getImageSourceFromType(card.type)} alt={card.name} />
-            </div>
-          </header>
-          <div className="card__image-wrapper">
-            <img className="card__image" src={getPokemeImage()} alt={card.name} />
-          </div>
-          <div className="card__anatomy">
-            <p className="card__anatomy-text">{`${card.type} PokéMe`}</p>
-            <p className="card__anatomy-text">{`Length: ${card.height.ft}' ${card.height.in}"`}</p>
-            <p className="card__anatomy-text">{`Weight: ${card.weight} lbs`}</p>
-          </div>
-          <ul className="card__attacks">{card.attacks.map((attack) => (
-            <li key={attack.name} className="card__attack">
-              <div className="card__attack-energy-cost"><EnergyCostIcons cost={attack.energyCost} type={attack.type} /></div>
-              <div className="card__attack-name-description"><span className="card__attack-name">{attack.name}</span> <span className="card__attack-description">{attack.description}</span></div>
-              <div className="card__attack-damage">{attack.damage}</div>
-            </li>
-            ))}
-          </ul>
-          <ul className="card__utility-stats">
-            <li className="card__utility-stat">
-              <span className="card__utility-stat-text">weakness</span>
-              <div className="card__icon type weakness">
-                <img className="icon small" src={getImageSourceFromType(card.weakness)} alt={card.weakness} width="100%" height="auto" />
+          <div
+            className="card card-wrapper"
+            role="button"
+            tabIndex="0"
+            onClick={toggleFlipHorizontally}
+          >
+            {flipped ? (
+              <div
+                role="button"
+                tabIndex="0"
+                className="card__back"
+                style={{ backgroundImage: "url(../images/card-bg.png)" }}
+              />
+            ) : (
+              <div
+                role="button"
+                tabIndex="0"
+                className={`card${card.isSpecial ? " special" : ""}`}
+                style={{ backgroundImage: getCardBackgroundImage() }}
+              >
+                <header className="card__header">
+                  <h3 className="card__name">{card.name}</h3>
+                  <div className="card__header-right">
+                    <span className="card__hit-points">{card.hitPoints}</span>
+                      <img className="card__type-badge icon" src={getImageSourceFromType(card.type)} alt={card.name} />
+                  </div>
+                </header>
+                <div className="card__image-wrapper">
+                  <img className="card__image" src={getPokemeImage()} alt={card.name} />
+                </div>
+                <div className="card__anatomy">
+                  <p className="card__anatomy-text">{`${card.type} PokéMe`}</p>
+                  <p className="card__anatomy-text">{`Length: ${card.height.ft}' ${card.height.in}"`}</p>
+                  <p className="card__anatomy-text">{`Weight: ${card.weight} lbs`}</p>
+                </div>
+                <ul className="card__attacks">{card.attacks.map((attack) => (
+                  <li key={attack.name} className="card__attack">
+                    <div className="card__attack-energy-cost"><EnergyCostIcons cost={attack.energyCost} type={attack.type} /></div>
+                    <div className="card__attack-name-description"><span className="card__attack-name">{attack.name}</span> <span className="card__attack-description">{attack.description}</span></div>
+                    <div className="card__attack-damage">{attack.damage}</div>
+                  </li>
+                  ))}
+                </ul>
+                <ul className="card__utility-stats">
+                  <li className="card__utility-stat">
+                    <span className="card__utility-stat-text">weakness</span>
+                    <div className="card__icon type weakness">
+                      <img className="icon small" src={getImageSourceFromType(card.weakness)} alt={card.weakness} width="100%" height="auto" />
+                    </div>
+                  </li>
+                  <li className="card__utility-stat resistance">
+                    <span className="card__utility-stat-text">resistance</span>
+                    <div className="card__icon type" />
+                  </li>
+                  <li className="card__utility-stat">
+                    <span className="card__utility-stat-text">retreat cost</span>
+                    <div className="card__icon type"><RetreatCostIcons cost={card.retreatCost} /></div>
+                  </li>
+                </ul>
+                <p className="card__description">{card.description}</p>
+                <footer className="card__footer">
+                  <span className="card__footer-text">Created by <a href="https://github.com/sprioleau">San&apos;Quan Prioleau</a> for <a href="http://cs52.me/">CS52</a></span>
+                </footer>
               </div>
-            </li>
-            <li className="card__utility-stat resistance">
-              <span className="card__utility-stat-text">resistance</span>
-              <div className="card__icon type" />
-            </li>
-            <li className="card__utility-stat">
-              <span className="card__utility-stat-text">retreat cost</span>
-              <div className="card__icon type"><RetreatCostIcons cost={card.retreatCost} /></div>
-            </li>
-          </ul>
-          <p className="card__description">{card.description}</p>
-          <footer className="card__footer">
-            <span className="card__footer-text">Created by <a href="https://github.com/sprioleau">San&apos;Quan Prioleau</a> for <a href="http://cs52.me/">CS52</a></span>
-          </footer>
-        </div>
-      </Tilt>
+            )}
+          </div>
+        </Tilt>
       {card && (
         <div className="message"><ReactMarkdown>{card.message}</ReactMarkdown></div>
       )}
--- src/app/components/Cards.js
+++ src/app/components/Cards.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import Tilt from "react-parallax-tilt";
 import { fetchCards } from "../store/actions/index";
 import { selectCards, selectFilter, selectIsLoading } from "../store/selectors";
 import Filter from "./Filter";
@@ -30,10 +31,18 @@ const Cards = () => {
           return (
             <li key={card.id} className="cards__list-item">
               <NavLink to={`/cards/${card.id}`}>
-                <div key={card.id} className="cards__card" style={{ backgroundImage: "url(../images/card-bg.png)" }}>
-                  <div className="cards__card-wrapper" />
-                </div>
-                <img className="cards__card-type-badge" src={`../images/pokeme-types/${card.type ? `${card.type.toLowerCase()}.svg` : "bug.svg"}`} alt={card.name} />
+                <Tilt
+                  className="parallax"
+                  perspective={1000}
+                  glareEnable
+                  glareMaxOpacity={0.45}
+                  scale={1.02}
+                >
+                  <div key={card.id} className="cards__card" style={{ backgroundImage: "url(../images/card-bg.png)" }}>
+                    <div className="cards__card-wrapper" />
+                  </div>
+                  <img className="cards__card-type-badge" src={`../images/pokeme-types/${card.type ? `${card.type.toLowerCase()}.svg` : "bug.svg"}`} alt={card.name} />
+                </Tilt>
                 <h4 className="cards__card-name" to={`/cards/${card.id}`}>{card.name}</h4>
               </NavLink>
             </li>
